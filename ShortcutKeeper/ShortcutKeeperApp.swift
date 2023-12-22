8ebf907da3eac7f9bfb5f973e0edc15bd04a2abd
Jeff Nawroth <jeff.nawroth@icloud.com>
2023-12-22 14:19:51 +0100

    Initial Commit

---
+++ ShortcutKeeper/ShortcutKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShortcutKeeperApp.swift
+//  ShortcutKeeper
+//
+//  Created by Jeff Nawroth on 22.12.23.
+//
+
+import SwiftUI
+
+@main
+struct ShortcutKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
